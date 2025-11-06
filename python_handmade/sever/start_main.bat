f925fb9f2a338a87f22bb098a6ff4ae695d9672f
thnthrbest <106542860+thnthrbest@users.noreply.github.com>
2025-11-06 15:54:55 +0700

    fixxxx

--- python_handmade/sever/start_main.bat
+++ python_handmade/sever/start_main.bat
@@ -2,6 +2,6 @@
 call "D:\MoveMunFunFresh\python_handmade\env\Scripts\activate.bat" REM Edit path
 python D:\MoveMunFunFresh\python_handmade\main.py REM Edit path
 
-echo Close in 10 Seconds
-TIMEOUT /T 10 /NOBREAK
+echo Close in 2 Seconds
+TIMEOUT /T 2 /NOBREAK
 exit
